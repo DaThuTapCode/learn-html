4c0f81b1be339d0944dfb4bf01375324ee37d220
Trong Phu <ntpdth2004@gmail.com>
2024-07-04 22:52:27 +0700

    Update buoi 1 va buoi hoc so 2

---
+++ profile-ntp/js/main.js
@@ -0,0 +1,4 @@
+document.getElementById('mobile-menu').addEventListener('click', function() {
+    const navLinks = document.querySelector('.navbar-links');
+    navLinks.classList.toggle('active');
+});
